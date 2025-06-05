5c8908c810552873ed7fdb172676bb5a805e704f
gettis <fedyuncev1133f5@gmail.com>
2025-06-05 14:29:52 +0300

    some changes routes

--- src/App/Providers/Router/ui/BrowserRouterApp.tsx
+++ src/App/Providers/Router/ui/BrowserRouterApp.tsx
@@ -6,29 +6,13 @@ import {
 } from 'react-router-dom';
 import LoaderPage from 'shared/ui/LoaderPage/LoaderPage';
 import { RouteConfig } from '../config/routeConfig';
+import RouterApp from './RouterApp';
 
 export function BrowserRouterApp() {
     return (
         <BrowserRouter>
             <Suspense fallback={<LoaderPage />}>
-                <Routes>
-                    {
-                        RouteConfig.map((route) => (
-                            <Route key={route.path} path={route.path} element={route.element}>
-                                {
-                                    route.children?.map((childRoute) => (
-                                        <Route
-                                            key={childRoute.path || 'idx'}
-                                            index={!!childRoute.index}
-                                            {...childRoute.path ? { path: childRoute.path } : {}}
-                                            element={childRoute.element}
-                                        />
-                                    ))
-                                }
-                            </Route>
-                        ))
-                    }
-                </Routes>
+                <RouterApp routeConfig={RouteConfig} />
             </Suspense>
         </BrowserRouter>
     );
